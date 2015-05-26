997b3944dec89d5271ae1f4ecf71fac8fd1d5f8a
Niklas Silfverström <niklas@silfverstrom.com>
2015-05-26 10:27:03 +0200

    added email validation

--- index.js
+++ index.js
@@ -1,48 +1,71 @@
 var _ = require('underscore');
 
+//RFC822 email validator
+var isEmail = require('./is_email')
+
 var StringValidator = {
     type : 'Duns-string-validator',
-    max : null,
-    min : null,
-    disallowed : [''],
-    allowed : [],
+    _clear : function() {
+        this.props = {
+            max : null,
+            min : null,
+            useEmail : null,
+            disallowed : [''],
+            allowed : [],
+            exactLength : null
+        };
+        return this;
+    },
     maxlen : function(max) {
-        this.max = max;
+        this.props.max = max;
         return this;
     },
     minlen : function(min) {
-        this.min = min;
+        this.props.min = min;
+        return this;
+    },
+    length : function(l) {
+        this.props.exactLength = l;
+        return this;
+    },
+    email : function() {
+        this.props.useEmail = true;
         return this;
     },
     minlen : function(min) {
-        this.min = min;
+        this.props.min = min;
         return this;
     },
     allow : function(val) {
         if(_(val).isArray() ) {
-            this.allowed = this.disallowed.concat(val);
+            this.props.allowed = this.disallowed.concat(val);
         } else {
-            this.allowed.push(val);
+            this.props.allowed.push(val);
         }
         return this;
     },
     deny : function(val) {
         if(_(val).isArray() ) {
-            this.disallowed = this.disallowed.concat(val);
+            this.props.disallowed = this.disallowed.concat(val);
         } else {
-            this.disallowed.push(val);
+            this.props.disallowed.push(val);
         }
         return this;
     },
     validate : function(param) {
+        var props = this.props;
         if(_(param).isString() === false) 
             throw "Not string";
-        if(_(this.disallowed).contains(param) && _(this.allowed).contains(param) === false )
+        if(_(props.disallowed).contains(param) && _(props.allowed).contains(param) === false )
             throw "Blacklisted value ";
-        if(this.max && param.length > this.max)
+        if(props.max && param.length > props.max)
             throw "Larger than allowed"
-        if(this.min && param.length < this.min)
+        if(props.min && param.length < props.min)
             throw "Larger than allowed"
+        if(props.exactLength && param.length !== props.exactLength)
+            throw "Invalid length"
+        if(props.useEmail && isEmail(param) === false) 
+            throw "Not RFC822 email"
     }
 };
 
@@ -74,7 +97,7 @@ var Duns = {
         return dschema;
     },
     string : function() {
-        var svalidator = Object.create(StringValidator);
+        var svalidator = Object.create(StringValidator)._clear();
         return svalidator;
     },
     validate : function(object, schema) {
@@ -82,16 +105,14 @@ var Duns = {
             var skey =  schema.get(key);
             if(skey && skey.type === 'Duns-string-validator') {
                 skey.validate(object[key]);
-                console.log('string!!', object[key]);
             }
         });
-        console.error('all good!');
     }
 };
 
 var TestSchema = Duns.schema({
-    name      : Duns.string().maxlen(10).minlen(5),
-    email     : Duns.string(),
+    name      : Duns.string().maxlen(10).minlen(5).length(6),
+    email     : Duns.string().email(),
     something : Duns.string().allow('')
 });
 var testobj  = {
